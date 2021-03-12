282440e3d8e1b4e9399b911c6b5395c10b4d2870
ethol <mrethol@gmail.com>
2021-03-12 18:44:43 +0100

    Source Code
    Source code of the ReCA experiments for the dynamic landscape of Reservoir Compuing with Elementary Cellular Automata

---
+++ ReCA parameter space/README.md
@@ -0,0 +1,7 @@
+# ReCA parameter space
+ 
+Experiment source code and archive for the Reservoir computing with cellular automata (ReCA)
+
+## Dependencies
+- EvoDynamics https://github.com/SocratesNFR/EvoDynamic
+- SciKit-learn https://github.com/scikit-learn/scikit-learn
---
+++ ReCA parameter space/helpers/helper.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def int_to_binary_string(int_number, size):
+    binary_array = list(str(int(bin(int_number)[2:])))
+    binary_array = list(map(int, binary_array))
+    return left_pad_array(binary_array, size)
+
+
+def left_pad_array(arr, size):
+    array = np.zeros(size, dtype=int)
+    array[-len(arr):] = arr
+    return array
+
+
+def pop_all_lists(list_of_lists):
+    firsts = []
+    for i in range(0, len(list_of_lists)):
+        firsts.append(list_of_lists[i].pop(0))
+    return firsts
+
+
+def flatten_list_of_lists(lists):
+    flat_list = []
+    for sublist in lists:
+        flat_list.extend(sublist)
+    return flat_list
